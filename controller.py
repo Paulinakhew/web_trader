b0dafd05cab5b7a2277d7054fbf3a872a6584413
Paulina Khew <paulinakhew12345@hotmail.com>
2019-09-05 21:35:37 -0400

    :fire: Remove old files

--- controller.py
+++ controller.py
@@ -19,10 +19,7 @@ def login():
         result = m.log_in(submitted_username,submitted_password)
         if result == True:
             username = submitted_username
-            if username=='admin':
-                return redirect('/adminmenu')
-            else:
-                return redirect('/menu')
+            return redirect('/menu')
         else:
             cannot_login = True
             return render_template('login.html', cannot_login=cannot_login)
@@ -37,17 +34,6 @@ def menu():
             return render_template('menu.html')
     else:
         return render_template('menu.html')
-
-@app.route('/adminmenu',methods=['GET','POST'])
-def adminmenu():
-    current_user = m.current_user()
-    if request.method=="GET":
-        if current_user != 'admin':
-            return redirect('/')
-        else:
-            return render_template('adminmenu.html')
-    else:
-        return render_template('adminmenu.html')
 
 @app.route('/create',methods=['GET','POST'])
 def create():
@@ -64,91 +50,6 @@ def create():
         else:
             cannot_create = True
             return render_template('create.html',cannot_create=cannot_create)
-
-@app.route('/lookup',methods=['GET','POST'])
-def look_up():
-    current_user = m.current_user()
-    if request.method=="GET":
-        if current_user == 'randomuser':
-            return redirect('/')
-        else:
-            return render_template('lookup.html')
-    else:
-        submitted_company_name=request.form['company_name']
-        ticker_symb = m.lookup_ticker_symbol(submitted_company_name)
-        result = "The ticker symbol for {} is {}.".format(submitted_company_name, ticker_symb)
-        #result = "The last price of {} is $".format(submitted_company_name)
-        return render_template('lookup.html',result=result)
-
-@app.route('/quote',methods=['GET','POST'])
-def quote():
-    current_user = m.current_user()
-    if request.method=="GET":
-        if current_user == 'randomuser':
-            return redirect('/')
-        else:
-            return render_template('quote.html')
-    else:
-        submitted_symbol=request.form['ticker_symbol']
-        price = m.quote_last_price(submitted_symbol)
-        result = "The last price of {} is ${}.".format(submitted_symbol, price)
-        return render_template('quote.html',result=result)
-
-@app.route('/buy',methods=['GET','POST'])
-def buy():
-    current_user = m.current_user()
-    if request.method=="GET":
-        if current_user == 'randomuser':
-            return redirect('/')
-        else:
-            return render_template('buy.html')
-    else:
-        submitted_symbol=request.form['ticker_symbol']
-        submitted_volume=request.form['number_of_shares']
-        submitted_volume = int(submitted_volume)
-        confirmation_message, return_list = m.buy(username,submitted_symbol,submitted_volume)
-        result = "You bought {} shares of {}.".format(submitted_volume, submitted_symbol)
-        m.update_holdings()
-        if confirmation_message == True:
-            m.buy_db(return_list)
-            return render_template('buy.html', result=result)
-        else:
-            return render_template('buy.html')
-
-@app.route('/sell',methods=['GET','POST'])
-def sell():
-    current_user = m.current_user()
-    if request.method=="GET":
-        if current_user == 'randomuser':
-            return redirect('/')
-        else:
-            return render_template('sell.html')
-    else:
-        submitted_symbol=request.form['ticker_symbol']
-        submitted_volume=request.form['number_of_shares']
-        submitted_volume = int(submitted_volume)
-        confirmation_message, return_list = m.sell(username,submitted_symbol,submitted_volume)
-        if submitted_volume < 2:
-            result = "You sold {} share of {}.".format(submitted_volume, submitted_symbol)
-        else:
-            result = "You sold {} shares of {}.".format(submitted_volume, submitted_symbol)
-        m.update_holdings()
-        if confirmation_message == True:
-            m.sell_db(return_list)
-            return render_template('sell.html', result=result)
-        else:
-            return render_template('sell.html')
-
-@app.route('/leaderboard',methods=['GET','POST'])
-def leaderboard():
-    current_user = m.current_user()
-    if request.method=="GET":
-        if current_user == 'randomuser':
-            return redirect('/')
-        else:
-            return render_template('leaderboard.html')
-    else:
-        return render_template('leaderboard.html')
 
 @app.route('/dashboard',methods=['GET','POST'])
 def dashboard():
