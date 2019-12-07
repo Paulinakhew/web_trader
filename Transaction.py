91abbb328a55fc01ef67942111672c8b22c4360a
Paulina Khew <paulinakhew12345@hotmail.com>
2019-12-06 21:59:12 -0500

    :tada: Initial commit of Transaction class
     #38

---
+++ Transaction.py
@@ -0,0 +1,22 @@
+class Transaction():
+    '''Class to store information about a single transaction'''
+    def __init__(
+        self,
+        user,
+        last_price,
+        brokerage_fee,
+        current_balance,
+        trade_volume,
+        left_over,
+        ticker_symbol,
+    ):
+        self.user = user
+        self.last_price = last_price
+        self.brokerage_fee = brokerage_fee
+        self.current_balance = current_balance
+        self.trade_volume = trade_volume
+        self.left_over = left_over
+        self.ticker_symbol = ticker_symbol
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
